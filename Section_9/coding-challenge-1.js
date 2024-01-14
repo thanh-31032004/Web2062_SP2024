f7c6754ff0adc02a491e0f1151a6de97765c2834
thanh-31032004 <vanthanhnguyen3103@gmail.com>
2024-01-14 10:19:23 +0700

    poject

---
+++ Section_9/coding-challenge-1.js
@@ -0,0 +1,65 @@
+const game ={
+    team1: 'Bayern Munich',
+    team2: 'Dortmund',
+    players: [
+    [    'Neuer',
+        'Pavard',
+        'Martinez',
+        'Alaba',
+        'Davies',
+        'Kimich',
+        'Goretzka',
+        'Coman',
+        'Muler',
+        'Gnarby',
+        'Lewandowski',
+    ],
+    [
+         'Bruki',
+         'Schulz',
+         'Hummels',
+         'Akanji',
+         'Hakimi',
+          'Weigl',
+          'Witsel',
+          'Hazard',
+          'Brandt',
+          'Sancho',
+          'Gotze',
+    ],
+],
+score : '4:0',
+scored: ['Lewandowski','Gnarby','Lewandowski','Hummels'],
+date: 'Nov 9th, 2037',
+odds: {
+    team1: 1.33,
+    x:3.25,
+    team2: 6.5,
+},
+};
+//1
+const [players1, players2] = game.players;
+console.log(players1,players2);
+//2
+const [gk, ...fieldPlayers] = players1;
+console.log(gk,fieldPlayers);
+//3
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+//4
+const playersFinal = [...players1, 'Thiago','Coutinho','Periscic'];
+//5
+const {odds: {
+    team1 , x :draw ,team2},
+} = game ;
+console.log(team1,draw,team2);
+//6
+const printGoals = function(...players){
+console.log(`${players.length} goals were scored`)
+}
+//printGoals('Davies','Muler','Lewandowski','Kimmich');
+
+printGoals(...game.scored);
+// 7
+team1 < team2 && console.log('Team1 is more likely to win');
+team1 > team2 && console.log('Team2 is more likely to win');
