95cf511ae69ac9b7083afed50a72fd1cd9142a1d
thanh-31032004 <vanthanhnguyen3103@gmail.com>
2024-01-28 06:45:05 +0700

    lab3,lab4

---
+++ Section_11/coding-challenge-2.js
@@ -0,0 +1,13 @@
+const calcAverageHumanAge = function(ages){
+    const hummanAges = ages.map(age => (age <= 2 ? 2* age: 16+ age*4))
+    const adults = hummanAges.filter(age => age >= 18);
+    console.log(hummanAges);
+    console.log(adults);
+
+    // const average = adults.reduce((acc,age) => acc+age, 0) / adults.length;
+    const average = adults.reduce((acc,age,i,arr)=> acc + age / arr.length, 0 );
+    return average
+}
+ const avg1=calcAverageHumanAge([5,2,4,1,15,8,3]);
+ const avg2 = calcAverageHumanAge([16,6,10,5,6,1,4]);
+ console.log(avg1,avg2);
